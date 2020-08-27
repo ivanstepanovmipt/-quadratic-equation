136a203e3b2cde9065d62c7d272c6186a36d7704
ivanstepanovmipt <57829855+ivanstepanovmipt@users.noreply.github.com>
2020-08-28 02:03:47 +0300

    Update qudratic.cpp

--- qudratic.cpp
+++ qudratic.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cmath>
 #include <assert.h>
+#include <cassert>
 
-const int Infinity = -1;
+const int Infinity_ROOTS = -1;
 
 int SolveQuadradicEquation(double a, double b, double c, double *x1, double *x2) {
     assert(x1);
@@ -26,12 +27,51 @@ int SolveQuadradicEquation(double a, double b, double c, double *x1, double *x2)
             *x1 = -c / b;
             return 1;
         } else {
-            return Infinity;
+            return Infinity_ROOTS;
         }
     }
 }
 
+void TestSolveQuadradicEquation() {
+
+    double test1_x1 = 0;
+    double test1_x2 = 0;
+    int test1_number_of_roots = SolveQuadradicEquation(1, 4, 3, &test1_x1, &test1_x2);
+    assert(test1_number_of_roots == 2);
+    assert(test1_x1 == -1 || test1_x1 == -3);
+    assert(test1_x2 == -1 || test1_x2 == -3);
+
+    double test2_x1 = 0;
+    double test2_x2 = 0;
+    int test2_number_of_roots = SolveQuadradicEquation(1, 2, 1, &test2_x1, &test2_x2);
+    assert(test2_number_of_roots == 1);
+    assert(test2_x1 == -1 || test2_x1 == -1);
+    assert(test2_x2 == -1 || test2_x2 == -1);
+
+    double test3_x1 = 0;
+    double test3_x2 = 0;
+    int test3_number_of_roots = SolveQuadradicEquation(0, 1, 2, &test3_x1, &test3_x2);
+    assert(test3_number_of_roots == 1);
+    assert(test3_x1 == -2 || test3_x1 == -2);
+    assert(test3_x2 == 0 || test3_x2 == 0);
+
+    double test4_x1 = 0;
+    double test4_x2 = 0;
+    int test4_number_of_roots = SolveQuadradicEquation(5, 1, 2, &test3_x1, &test3_x2);
+    assert(test4_number_of_roots == 0);
+    assert(test4_x1 == 0 || test4_x1 == 0);
+    assert(test4_x2 == 0 || test4_x2 == 0);
+
+    double test5_x1 = 0;
+    double test5_x2 = 0;
+    int test5_number_of_roots = SolveQuadradicEquation(0, 0, 0, &test3_x1, &test3_x2);
+    assert(test5_number_of_roots == -1);
+    assert(test5_x1 == 0 || test5_x1 == 0);
+    assert(test5_x2 == 0 || test5_x2 == 0);
+}
+
 int main () {
+    TestSolveQuadradicEquation();
     double a = 0;
     double b = 0;
     double c = 0;
