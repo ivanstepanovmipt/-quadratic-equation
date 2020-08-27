9330493cb8cac2a34d42ef4e9d587e1c64187d82
ivanstepanovmipt <57829855+ivanstepanovmipt@users.noreply.github.com>
2020-08-28 00:37:10 +0300

    Update qudratic.cpp

--- qudratic.cpp
+++ qudratic.cpp
@@ -2,16 +2,22 @@
 #include <cmath>
 #include <assert.h>
 
+const int Infinity = -1;
+
 int SolveQuadradicEquation(double a, double b, double c, double *x1, double *x2) {
     assert(x1);
     assert(x2);
     assert(x1 != x2);
     if(a != 0){
         double discriminant = b * b - 4 * a * c;
-        if(discriminant >= 0){
+        if(discriminant > 0){
             *x1 = (-b + sqrt(discriminant)) / (2 * a);
             *x2 = (-b - sqrt(discriminant)) / (2 * a);
             return 2;
+        } else if (discriminant == 0) {
+            *x1 = -b / (2 * a);
+            *x2 = -b / (2 * a);
+            return 1;
         } else {
             return 0;
         }
@@ -20,7 +26,7 @@ int SolveQuadradicEquation(double a, double b, double c, double *x1, double *x2)
             *x1 = -c / b;
             return 1;
         } else {
-            return 0;
+            return Infinity;
         }
     }
 }
@@ -32,8 +38,26 @@ int main () {
     double x1 = 0;
     double x2 = 0;
     int number_of_roots = 0;
-    std::cin >> a >> b >> c;
+    std::cout << "enter coefficient at x^2";
+    std::cin >> a;
+    std::cout << "enter coefficient at x";
+    std::cin >> b;
+    std::cout << "enter coefficient at x^0";
+    std::cin >> c;
     number_of_roots = SolveQuadradicEquation(a, b, c, &x1, &x2);
-    std::cout << number_of_roots << " " << x1 << " " << x2;
+    switch(number_of_roots){
+    case 2:
+        std::cout << "x1 = : " << x1 << " x2 = : " << x2;
+        break;
+    case 1:
+        std::cout << "x = : " << x1;
+        break;
+    case 0:
+        std::cout << "no solutions";
+        break;
+    case -1:
+        std::cout << "infinitely many solutions";
+        break;
+    }
     return 0;
 }
